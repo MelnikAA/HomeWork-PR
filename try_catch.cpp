19419b1730f430878526dbe1c09177af3c6e1e5f
MelnikAA <85779525+MelnikAA@users.noreply.github.com>
2022-05-05 09:40:44 +1000

    Add files via upload

---
+++ try_catch.cpp
@@ -0,0 +1,64 @@
+﻿// try_catch.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class A
+{
+public:
+	double Re;
+	double Lm;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	bool exit = true;
+	A complex;
+	while (exit)
+	{
+		try
+		{
+			cout << "Введите веществ часть: ";
+			cin >> complex.Re;
+			if (cin.fail()) {
+				throw 1;
+			}
+			cout << "\nВведите мним часть: ";
+			cin >> complex.Lm;
+			if (cin.fail()) {
+				throw "fail";
+			}
+			exit = false;
+		}
+		catch (int er)
+		{
+			cout << "\nВсе фигня, давай по новой\n" << endl;
+			cin.clear();
+			cin.ignore();
+		}
+		catch (const char* er)
+		{
+			cout << "\nОшибка типа char*" << endl;
+
+			cin.clear();
+			cin.ignore();
+		}
+		catch (...)
+		{
+			std::cout << "\nНекорректный ввод данных. Повторите попытку." << endl;
+
+			cin.clear();
+			cin.ignore();
+		}
+	}
+
+	cout << "Вы ввели " << complex.Re << ' ' << complex.Lm << endl;
+
+	system("pause");
+}
